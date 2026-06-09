38563f7367f6f9380bc2faf046906f4283c54afb
bergoniawilliam <bergoniawilliam@gmail.com>
2026-06-09 10:23:36 +0800

    with admin and migration2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 /**
  * @extends Factory<User>
